a72f7d494436d9f77431537b4219269bc2168365
sharan1u <37862017+sharan1u@users.noreply.github.com>
2018-10-16 16:18:44 +0530

    Update settings.py

--- tanujbeta/settings.py
+++ tanujbeta/settings.py
@@ -81,13 +81,8 @@ WSGI_APPLICATION = 'tanujbeta.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        #'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-        'NAME':'tanuj_db',
-        'USER':'ujjwal',
-        'PASSWORD':'lush8809',
-        'HOST':'127.0.0.1',
-        'PORT':'5432'
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
